3f1ca3ae780db203f7687469631be477fda4533b
SAALAIKUSHAL <121267459+SAALAIKUSHAL@users.noreply.github.com>
2023-05-10 14:30:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('inptBox');
+let buttons = document.querySelectorAll('button');
+window.addEventListener("keydown", function(event) {
+  if (!event.key.match(/[0-9]/)) {
+    showAlert("Only numbers");
+  }
+});
+
+function showAlert(message) {
+  alert(message);
+}
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      string = eval(string);
+      input.value = string;
+
+    } else if (e.target.innerHTML == 'AC') {
+      string = " ";
+      input.value = string;
+
+    } else if (e.target.innerHTML == 'DEL') {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  })
+});
